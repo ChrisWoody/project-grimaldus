6e1396ff2a7458f7abbfe4f22a8663659920395b
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2018-10-09 20:47:36 +0800

    And again

--- deploy.ps1
+++ deploy.ps1
@@ -19,7 +19,7 @@ try {
     $msDeployArgs =
     '-verb:sync ' +
     "-source:package='$PackagePath' " + 
-    "-dest:ComputerName=https://184.173.161.69:8172/MSDeploy.axd?site=chriswoodcodes.net,UserName=$Username,Password=$Password,AuthType='Basic',includeAcls='False' " +
+    "-dest:auto,ComputerName=https://184.173.161.69:8172/MSDeploy.axd?site=chriswoodcodes.net,UserName=$Username,Password=$Password,AuthType='Basic',includeAcls='False' " +
     "-retryAttempts:5 -retryInterval:5000 " +
     "-allowUntrusted"
     
