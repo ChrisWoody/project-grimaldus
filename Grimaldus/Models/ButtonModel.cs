f11186571f03d5eab9fc03ac5a6f4486e919029c
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2019-02-19 06:48:11 +0800

    Removing everything in prep for github pages

--- Grimaldus/Models/ButtonModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Grimaldus.Models
-{
-    public class ButtonModel
-    {
-        public string Link { get; set; }
-        public string Text { get; set; }
-        public bool OpenInNewTab { get; set; }
-    }
-}
